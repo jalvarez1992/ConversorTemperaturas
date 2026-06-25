a670de53959b5da5e31ffa8d1bee2c8619771f51
Jorge Alvarez <jlalvarez@uth.hn>
2026-06-24 20:07:10 -0600

    Cambios

--- src/main/java/com/mycompany/conversortemperaturas/ConversorTemperaturas.java
+++ src/main/java/com/mycompany/conversortemperaturas/ConversorTemperaturas.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.conversortemperaturas;
 
-/**
- *
- * @author 
- */
+
 public class ConversorTemperaturas {
 
     public static void main(String[] args) {
