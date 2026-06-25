b036856d6b2ff9adf799590b26431b21feb5a1ba
Gregory <Gregory@Asus>
2026-06-24 20:26:34 -0600

    Hola

--- src/main/java/com/mycompany/conversortemperaturas/ConversorTemperaturas.java
+++ src/main/java/com/mycompany/conversortemperaturas/ConversorTemperaturas.java
@@ -6,5 +6,7 @@ public class ConversorTemperaturas {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hola soy Greg");
     }
+    
 }
